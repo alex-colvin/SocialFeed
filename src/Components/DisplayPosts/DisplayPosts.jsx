78b319efec7940a28c554c349e2a47d6f2ed449b
alex-colvin <alexcolvin@live.com>
2022-05-03 16:35:03 -0400

    Added Like/Dislike butttons to Post component

--- src/Components/DisplayPosts/DisplayPosts.jsx
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -3,18 +3,16 @@ import Post from '../Post/Post';
 const DisplayPosts = (props) => {
     return (
         <>
-        <h1>Feed</h1> 
-        
-          <body>
-              {props.posts.map((entry, index) => {
-              
+        <h1>Feed</h1>         
+          <>
+              {props.posts.map((entry, index) => {              
                 return(
                 <div key={index}>
                     <Post name={entry.name} post={entry.post}/>
                 </div>
                 )
               })}
-          </body>
+          </>
           </>       
      );
 }
--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,12 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import './Post.css'
 
 const Post = (props) => {
+
+    const [like, setLike] = useState('Like')
+    const [dislike, setDislike] = useState('Dislike')
+    const [likeTrue, setLikeTrue] = useState(false);
+    const [dislikeTrue, setDislikeTrue] = useState(false);
+
+    useEffect(() => {
+        if(likeTrue){
+            setLike('Liked')
+            setDislikeTrue(false)
+        }
+        else{
+            setLike('Like')
+        }
+    }, [likeTrue]);
+
+    useEffect(() => {
+        if(dislikeTrue){
+            setDislike('Disliked')
+            setLikeTrue(false)
+        }
+        else{
+            setDislike('Dislike')
+        }
+    }, [dislikeTrue]);
+    
+    function likePost(){
+       setLikeTrue(!likeTrue)
+    }
+
+    function dislikePost(){
+        setDislikeTrue(!dislikeTrue)
+    }
+    
+
+
     return(
         <>
             <div>{props.name}</div>
             <div>{props.post}</div>          
             <div>
-                <button>Like</button>
-                <button>Not Like</button>
+                <button className={likeTrue ? 'likeButton': null} onClick={() => {likePost()}}>{like}</button>
+                <button className={dislikeTrue ? 'dislikeButton': null}  onClick={() => {dislikePost()}}>{dislike}</button>
             </div>
         </>
     );
